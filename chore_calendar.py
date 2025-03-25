cd50f012dd7abd31601becb5dc1989d7a0091d44
curranmax <curranmax@gmail.com>
2025-03-25 02:36:15 +0000

    Disable temporary code.

--- chore_calendar.py
+++ chore_calendar.py
@@ -449,10 +449,7 @@ class ChoreCalendar:
         f.close()
 
         # Update the chores in case there are any compatibility issues (i.e. add any new fields to old data).
-        for _, chore in loaded_chores.items():
-            chore.is_pending = False
-
-        update_last_post = True
+        update_last_post = False
         if update_last_post:
             print('Starting update_last_post for saved chores.')
             cur_date = min(chore.start_date for _, chore in loaded_chores.items()) - timedelta(days=1)
